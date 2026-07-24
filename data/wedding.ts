d84176a71fa08fde229a4401ca8b71d4fbea78b8
ratna182 <byebyanbye@gmail.com>
2026-07-24 17:47:55 +0700

    fix cover webp path

--- data/wedding.ts
+++ data/wedding.ts
@@ -1,29 +1,19 @@
 export const wedding = {
-    hero: {
-        title: "The Wedding Of",
-        image: "/images/cover.jpg",
-    },
-
-
-    greeting:
-        "Dengan penuh kebahagiaan, kami mengundang Bapak/Ibu/Saudara/i untuk hadir dalam hari istimewa kami.",
-
 
     bride: {
         firstName: "Ratna",
-        fullName: "Ratna Ayu",
-        photo: "/images/bride.jpg",
-        father: "Bapak Nama Ayah",
-        mother: "Ibu Nama Ibu",
+        fullName: "Ratna Putri",
     },
 
 
     groom: {
         firstName: "Andi",
         fullName: "Andi Pratama",
-        photo: "/images/groom.jpg",
-        father: "Bapak Nama Ayah",
-        mother: "Ibu Nama Ibu",
+    },
+
+
+    cover: {
+        image: "/images/cover.webp",
     },
 
 
@@ -32,48 +22,39 @@ export const wedding = {
         day: "Saturday",
         time: "09.00 WIB",
 
+        countdownDate:
+            "2027-06-20T09:00:00",
+
         location:
-            "Gedung Pernikahan, Jakarta Indonesia",
+            "Ballroom Wedding Venue",
 
         maps:
             "https://maps.google.com",
-
-        countdownDate:
-            "2027-06-20T09:00:00",
-    },
-
-
-    story: {
-        firstMeet: "2019",
-        engagement: "2026",
-        wedding: "2027",
     },
 
 
     gallery: [
-        "/images/gallery-1.jpg",
-        "/images/gallery-2.jpg",
-        "/images/gallery-3.jpg",
-        "/images/gallery-4.jpg",
-        "/images/gallery-5.jpg",
-        "/images/gallery-6.jpg",
+        "/images/gallery-1.webp",
+        "/images/gallery-2.webp",
+        "/images/gallery-3.webp",
     ],
 
 
     gift: {
         bank: "BCA",
 
-        accountNumber:
+        account:
             "1234567890",
 
-        accountName:
-            "Ratna Ayu",
+        name:
+            "Ratna Putri",
     },
 
 
-    rsvp: {
-        whatsapp:
-            "628123456789",
+    music: {
+        src:
+            "/audio/music.mp3",
     },
+
 
 };
